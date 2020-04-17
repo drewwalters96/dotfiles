8ba63d282eadb59353c52438e06a30120df9fd0f
Drew Walters <andrew.walters@att.com>
2020-04-17 18:57:12 +0000

    minor fixes

--- tools/00-initial-setup.sh
+++ tools/00-initial-setup.sh
@@ -3,8 +3,8 @@
 set -x
 
 if [[ ! "$(which ansible)" ]]; then
-  apt update
-  apt install software-properties-common
-  apt-add-repository --yes --update ppa:ansible/ansible
-  apt install -y --no-install-recommends ansible
+  sudo -H apt update
+  sudo -H apt install software-properties-common
+  sudo -H apt-add-repository --yes --update ppa:ansible/ansible
+  sudo -H apt install -y --no-install-recommends ansible
 fi
