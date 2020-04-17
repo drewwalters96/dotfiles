9143b7ddbfc556091e738be79ad1be83381d2dba
Drew Walters <drewwalters96@gmail.com>
2020-04-17 15:19:28 -0500

    Cleanup tools dir
    Install by running tools/ubuntu.sh
    
    Signed-off-by: Drew Walters <drewwalters96@gmail.com>

--- tools/05-run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -x
-
-ANSIBLE_CFG="$(pwd)/ansible.cfg" ansible-playbook -c local \
-  -e @playbooks/vars/default.yaml playbooks/system-setup/main.yaml
--- tools/ubuntu.sh
+++ tools/ubuntu.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+#
+# Install dotfiles on Ubuntu
 
-set -x
-
+# Install ansible
 if [[ ! "$(which ansible)" ]]; then
   sudo -H apt update
   sudo -H apt install software-properties-common
   sudo -H apt-add-repository --yes --update ppa:ansible/ansible
   sudo -H apt install -y --no-install-recommends ansible
 fi
+
+# Run system setup playbook
+ANSIBLE_CFG="$(pwd)/ansible.cfg" ansible-playbook -c local \
+  -e @playbooks/vars/default.yaml playbooks/system-setup/main.yaml
